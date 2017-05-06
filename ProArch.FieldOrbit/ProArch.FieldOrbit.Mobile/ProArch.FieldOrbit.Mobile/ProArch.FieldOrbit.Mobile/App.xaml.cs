d5f724cb9d8696fdd92b8e2ffd70379d74bfaf2c
Naveen Hegde <naveenh1@sedata.com>
2017-05-06 19:47:17 +0530

    Initial commit
    Signed-off-by: Naveen Hegde <naveenh1@sedata.com>

---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ProArch.FieldOrbit.Mobile.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ProArch.FieldOrbit.Mobile
+{
+    public partial class App : Application
+    {
+        public static bool IsUserLoggedIn { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            SetMainPage();
+        }
+
+        public static void SetMainPage()
+        {
+            Current.MainPage = new NavigationPage(new LoginPage())
+            {
+                Title = "Field Orbit",
+                Icon = Device.OnPlatform("tab_feed.png", null, null)
+            };
+        }
+    }
+}
---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProArch.FieldOrbit.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var isValid = true;
+
+
+                if (isValid)
+                {
+                    App.IsUserLoggedIn = true;
+                    Navigation.InsertPageBefore(new JobsPage(), this);
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    //messageLabel.Text = "Login failed";
+                }
+            }
+            catch (Exception Exception)
+            {
+
+
+            }
+        }
+
+
+        private void btnCancel_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
