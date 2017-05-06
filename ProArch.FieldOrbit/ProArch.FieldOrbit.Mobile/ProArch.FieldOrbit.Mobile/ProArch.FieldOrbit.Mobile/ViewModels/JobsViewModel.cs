15b98271f141065055d22bb7a066b6e7fc419e7b
Naveen Hegde <naveenh1@sedata.com>
2017-05-06 19:47:17 +0530

    Initial commit
    Signed-off-by: Naveen Hegde <naveenh1@sedata.com>

---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/ViewModels/JobsViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using ProArch.FieldOrbit.Mobile.Helpers;
+using ProArch.FieldOrbit.Mobile.Models;
+using ProArch.FieldOrbit.Mobile.Views;
+
+using Xamarin.Forms;
+using ProArch.FieldOrbit.Mobile.Services;
+using System.Collections.Generic;
+
+namespace ProArch.FieldOrbit.Mobile.ViewModels
+{
+    public class JobsViewModel : BaseViewModel
+    {
+        public ObservableRangeCollection<Ticket> Jobs { get; set; }
+        public Command LoadJobsCommand { get; set; }
+
+        public JobsViewModel()
+        {
+            Title = "Jobs";
+            Jobs = new ObservableRangeCollection<Ticket>();
+            LoadJobsCommand = new Command(async () => await ExecuteLoadTicketsCommand());
+        }
+
+        async Task ExecuteLoadTicketsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Jobs.Clear();
+                var tickets = await ServiceAdapter.Instance.Get<List<Ticket>>("GetTickets");
+                Jobs.ReplaceRange(tickets);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                MessagingCenter.Send(new MessagingCenterAlert
+                {
+                    Title = "Error",
+                    Message = "Unable to load items.",
+                    Cancel = "OK"
+                }, "message");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/Views/JobsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using ProArch.FieldOrbit.Mobile.Models;
+using ProArch.FieldOrbit.Mobile.ViewModels;
+
+using Xamarin.Forms;
+
+namespace ProArch.FieldOrbit.Mobile.Views
+{
+    public partial class JobsPage : ContentPage
+    {
+        JobsViewModel viewModel;
+
+        public JobsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new JobsViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Ticket;
+            if (item == null)
+                return;
+
+            Navigation.PushAsync(new JobDetailsPage());
+
+            // Manually deselect item
+            JobsListView.SelectedItem = null;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Jobs.Count == 0)
+                viewModel.LoadJobsCommand.Execute(null);
+        }
+    }
+}
