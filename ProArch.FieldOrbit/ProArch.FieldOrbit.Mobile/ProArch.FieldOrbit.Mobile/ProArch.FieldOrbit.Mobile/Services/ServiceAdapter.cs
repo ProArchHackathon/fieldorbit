8d5e8c6797a1885233db987831809f568cd85dab
Naveen Hegde <naveenh1@sedata.com>
2017-05-06 19:47:17 +0530

    Initial commit
    Signed-off-by: Naveen Hegde <naveenh1@sedata.com>

---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/Services/ServiceAdapter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ProArch.FieldOrbit.Mobile.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ProArch.FieldOrbit.Mobile.Services
+{
+    internal class ServiceAdapter
+    {
+        private const string API_URI = "http://192.168.21.117/HackathonMock/api/";
+
+        private HttpClient client;
+        private static ServiceAdapter instance;
+
+        private ServiceAdapter()
+        {
+            client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+        }
+
+        public static ServiceAdapter Instance
+        {
+            get
+            {
+                if (instance == null) instance = new ServiceAdapter();
+
+                return instance;
+            }
+        }
+    
+
+        public async Task<T> Get<T>(string path)
+        {
+            string restUrl = API_URI + path;
+            var uri = new Uri(restUrl, UriKind.Absolute);
+
+            try
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(content.Result);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message + Environment.NewLine + e.StackTrace);
+            }
+
+            return default(T);
+        }
+
+        public async Task Post<T>(string path, object o)
+        {
+            if (o == null) return;
+
+            string restUrl = API_URI + path;
+            var uri = new Uri(restUrl, UriKind.Absolute);
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(o);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(uri, content);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message + Environment.NewLine + e.StackTrace);
+            }
+        }
+
+
+    }
+}
---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/ProArch.FieldOrbit.Mobile/Views/CreateJobPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProArch.FieldOrbit.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateJobPage : ContentPage
+    {
+        public CreateJobPage()
+        {
+            InitializeComponent();
+            FillStatusCombo();
+
+        }
+
+        private void FillStatusCombo()
+        {
+            cboStatus.Items.Add("Started");
+            cboStatus.Items.Add("InProgress");
+            cboStatus.Items.Add("Completed");
+        }
+
+        private void cboStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboPriority_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
