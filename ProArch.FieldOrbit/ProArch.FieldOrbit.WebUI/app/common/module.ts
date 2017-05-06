56b1fdc24f0f9cdfd1175a2d41c8f2558b328014
KiranA <kirana@proarch.com>
2017-05-06 13:58:25 +0530

    setting up angular in webui project

---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.WebUI/app/common/module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { JobComponent } from '../components/job/job.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        JobComponent
+    ],
+    bootstrap: [JobComponent]
+})
+export class AppModule { }
