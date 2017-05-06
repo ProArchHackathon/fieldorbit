10d7a40c7e70d34befb50bc8dedb96c87f5e3718
KiranKumarT <kirankumart@proarchitsolutions.com>
2017-05-06 18:30:02 +0530

    Missed Added files - Model Finalized

---
+++ ProArch.FieldOrbit/ProArch.FieldOrbit.Models/User.cs
@@ -0,0 +1,13 @@
+﻿using ProArch.FieldOrbit.Models.Enums;
+
+namespace ProArch.FieldOrbit.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public EmployeeType Type { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
